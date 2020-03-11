c8451600e9ec6f087e9d04c17c8dc964e3835f43
Amanda Hogan <ahogan@teamworks.com>
2020-03-11 11:19:48 -0400

    Setup custom middleware functions

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,22 @@
 const express = require('express');
 
+const Posts = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
@@ -22,6 +24,16 @@ router.put('/:id', (req, res) => {
 
 function validatePostId(req, res, next) {
   // do your magic!
+  Posts.getById(req.params.id)
+  .then(post => {
+    if (!post) {
+      res.status(400).json({ message: "Invalid post ID" });
+    } else {
+      res.status(200).json(post);
+    };
+  });
+
+  next();
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const Users = require('./userDb');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
 });
 
@@ -14,19 +16,19 @@ router.get('/', (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
 });
 
@@ -34,14 +36,38 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  Users.getById(req.params.id)
+    .then(user => {
+      if (!user) {
+        res.status(400).json({ message: "Invalid user ID" });
+      } else {
+        res.status(200).json(user);
+      };
+    });
+
+  next();
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "Missing user data" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "Missing required name field" });
+  };
+
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "Missing post data" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "Missing required text field" });
+  };
+
+  next();
 }
 
 module.exports = router;
