22f15b0b1b2e06ea087d772b64e925fcd8be01cd
Amanda Hogan <ahogan@teamworks.com>
2020-03-12 14:15:23 -0400

    Start script and dotenv library

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const server = require('./server');
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+server.listen(port, () => console.log(`API running on http://localhost:${port}`));
